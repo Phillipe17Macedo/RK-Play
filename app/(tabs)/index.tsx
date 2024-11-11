226b23af7bd21e33ef832dbda95d0c53b152d8f3
Phillipe Macedo <63377161+Phillipe17Macedo@users.noreply.github.com>
2024-11-11 15:28:03 -0300

    ajustando-tela-index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,23 @@
 import { styles } from "@/src/styles/index/styles";
 
 import EditScreenInfo from "@/src/components/EditScreenInfo";
-import { Text, View } from "@/src/components/Themed";
+import { View } from "@/src/components/Themed";
+
+// components
+import { OpcoesTelaInicial } from "@/src/components/pages/inicial/opcoesTelaInicial";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <View
+      {/* <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      /> */}
+
+      <OpcoesTelaInicial />
+
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
